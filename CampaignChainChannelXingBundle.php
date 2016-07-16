4cc62a258c46e4629a408ccf6256309ea56ecf1e
Sandro Groganz <sandro@campaignchain.com>
2016-07-16 12:32:20 -0700

    CampaignChain/campaignchain#192 Subsume all app-wide parameters under campaignchain_core using tree builder

--- CampaignChainChannelXingBundle.php
+++ CampaignChainChannelXingBundle.php
@@ -2,8 +2,13 @@
 
 namespace CampaignChain\Channel\XingBundle;
 
+use CampaignChain\Channel\XingBundle\DependencyInjection\CampaignChainChannelXingExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CampaignChainChannelXingBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new CampaignChainChannelXingExtension();
+    }
 }
